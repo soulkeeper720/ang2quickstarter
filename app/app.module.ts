ea0317f2e7b0d2e4bead18ba2ceb94d163c97b63
Santosh Misal <santosh@shopyourworld.com>
2017-01-11 19:49:47 +0530

    changes

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { NavbarComponent }  from './components/navbar/navbar.components';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, NavbarComponent ],
+  bootstrap:    [ AppComponent, NavbarComponent ]
 })
 export class AppModule { }
